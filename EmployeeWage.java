9bec46407b0065129ea0915ac6a9ca2086e31fd1
Ansh Mittal <mittalansh22@gmail.com>
2020-07-18 13:36:35 +0530

    added: part time or full time

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,18 +4,26 @@ class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Program");
 
-		final int isPresent=1;
-		int empHour=8;
+		int empHour=0;
 		int empRatePerHour=20;
 		int empSalary=0;
+		int partTime=0;
+		int fullTime=1;
 
 		double empPresent = Math.floor(Math.random() * 10) % 2;
-		if ( empPresent == isPresent ){
-			System.out.println("Employee is Present");
-			empSalary=empRatePerHour*empHour;
+		
+		if ( empPresent == partTime ){
+			System.out.println("Employee is part time Present");
+			empHour=4;
 		}
-		else
+		
+		if(empPresent == fullTime){
 			System.out.println("Employee is Absent");
+			empHour=8;
+		}
+
+		empSalary=empRatePerHour*empHour;
+		
 		System.out.println("Salary is :" + empSalary);
 	}
 }
