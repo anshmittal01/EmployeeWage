d2952ac6a2771f98f45234a0c0f18242a03efd4e
Ansh Mittal <mittalansh22@gmail.com>
2020-07-18 10:47:00 +0530

    Modify:EmployeeSalary to calculate daily wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,11 +5,17 @@ class EmployeeWage {
 		System.out.println("Welcome to Employee Wage Program");
 
 		final int isPresent=1;
-		
+		int empHour=8;
+		int empRatePerHour=20;
+		int empSalary=0;
+
 		double empPresent = Math.floor(Math.random() * 10) % 2;
-		if ( empPresent == isPresent )
+		if ( empPresent == isPresent ){
 			System.out.println("Employee is Present");
+			empSalary=empRatePerHour*empHour;
+		}
 		else
 			System.out.println("Employee is Absent");
+		System.out.println("Salary is :" + empSalary);
 	}
 }
